3d6ef9de46d64edef0a0da730514be982001b837
Thushara <thusharan@hotmail.com>
2022-05-23 09:12:39 -0400

    refactor InterviewerListItem to use props instead of hard-coded values

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
+export default function InterviewerListitem(props) {
 
-export default function InterviewerListitem(props) {  
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
 
+  const imgClass = classNames({ //conditional SCSS for if selected/not
+    "interviewers__item-image": props.avatar,
+    "interviewers__item-image--selected-image":props.avatar && props.selected
+  })
+
+  function formatName(name,selected){ //conditionally renders name 
+    if (selected) {
+      return `${name}`
+    }
+  }
   return (
-    <li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
+    <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}> 
+      <img
+        className={imgClass}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {formatName(props.name, props.selected)}
+    </li>
   )
 }
