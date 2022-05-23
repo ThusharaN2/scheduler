d3bbc9fe2bae1de807f6035d28da01900f089110
Thushara <thusharan@hotmail.com>
2022-05-23 09:55:51 -0400

    refactor InterviewerList by using map() & props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
 
-export default function InterviewerListitem(props) {
+export default function InterviewerList(props) {
 
-return (
-  <section className="interviewers">
-  <h4 className="interviewers__header text--light">Interviewer</h4>
-  <ul className="interviewers__list"></ul>
-</section>
-)
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+
+        {props.interviewers.map((theInterviewer) => (
+          <InterviewerListItem
+            key={theInterviewer.id}
+            id={theInterviewer.id}
+            name={theInterviewer.name}
+            avatar={theInterviewer.avatar}
+            selected={theInterviewer.id === props.interviewer}
+            setInterviewer={() => props.setInterviewer(theInterviewer.id)} //set interviewer  w/ anon fcn declaration so it's not called right away
+            />
+        ))}
+      </ul>
+    </section>
+  )
 }
--- stories/index.js
+++ stories/index.js
@@ -105,6 +105,8 @@ storiesOf("Button", module)
         />
       ));
 
+
+      
       const interviewers = [
         { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
         { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
