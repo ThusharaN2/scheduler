e4eb7e2e79eb29a3f348d17b94f5471ac3822827
Thushara <thusharan@hotmail.com>
2022-05-24 22:24:47 -0400

    use promise instead of axios to retrieve appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,49 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-
-
-const appointments = {     //data is constant so shouldn't be w/in application component
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
-
-
+import { getAppointmentsForDay } from "helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -56,49 +14,58 @@ export default function Application(props) {
     appointments: {}
   });
 
+  // const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   const setDay = day => setState({ ...state, day });
-  const setDays = (days)=>{ setState({...state, days})}
+  // const setDays = (days) => {
+  //   setState(prev => ({ ...prev, days }));
 
 
-  useEffect(() => {
-    axios.get("/api/days")
-    .then((res) => {
-      setDays(res.data)
-    })
+    useEffect(() => {
+      Promise.all([
+        axios.get("/api/days"),
+        axios.get("/api/appointments"),
+        axios.get("/api/interviewers")
+      ]).then(all => {
+        console.log(all[0].data)
+        console.log(all[1].data)
+        console.log(all[2].data)
+        setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      })
+    }, [])
 
-  },[])
-
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
-        />
-        <hr className="sidebar__separator sidebar--centered" />
-        <nav className="sidebar__menu">
-          <DayList
-            days={state.days}
-            value={state.day} //getter
-            onChange={setDay} //setter
+    return (
+      <main className="layout">
+        <section className="sidebar">
+          <img
+            className="sidebar--centered"
+            src="images/logo.png"
+            alt="Interview Scheduler"
           />
-        </nav>
-        <img
-          className="sidebar__lhl sidebar--centered"
-          src="images/lhl.png"
-          alt="Lighthouse Labs"
-        />      </section>
-      <section className="schedule">
+          <hr className="sidebar__separator sidebar--centered" />
+          <nav className="sidebar__menu">
+            <DayList
+              days={state.days}
+              value={state.day} //getter
+              onChange={setDay} //setter
+            />
+          </nav>
+          <img
+            className="sidebar__lhl sidebar--centered"
+            src="images/lhl.png"
+            alt="Lighthouse Labs"
+          />      </section>
+        <section className="schedule">
 
-      {Object.values(appointments).map(appointment =>  //uses Object.values() to change appts into an array to be mapped
-          <Appointment
-          key={appointment.id} 
-          {...appointment}
-          /> //represents last appt of day(check console to see if appt has key)
-        )} 
-        <Appointment key="last" time="5pm" />      
+          {dailyAppointments.map(appointment =>  //uses Object.values() to change appts into an array to be mapped
+            <Appointment
+              key={appointment.id}
+              {...appointment}
+            /> //below represents last appt of day(check console to see if appt has key)
+          )}
+          <Appointment key="last" time="5pm" />
         </section>
-    </main>
-  );
-}
+      </main>
+    );
+  }
+
